cf954eab9f81cb36d44123ab85b8bede67b04403
Philipp Michael Arbeitstein <philippar@edu.aau.at>
2024-03-05 18:29:12 +0100

    Added Socket and Thread integration with ClickListener

--- project/app/src/main/java/com/arbeitstein/myapplication/MainActivity.java
+++ project/app/src/main/java/com/arbeitstein/myapplication/MainActivity.java
@@ -1,16 +1,19 @@
 package com.arbeitstein.myapplication;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,5 +33,33 @@ public class MainActivity extends AppCompatActivity {
         tvResponse = findViewById(R.id.tvResponse);
 
         setSupportActionBar(toolbar);
+
+
+        serverButton.setOnClickListener(view -> sendMatNrToServer(editTextNumber.getText().toString()));
     }
+
+    private void sendMatNrToServer(String matNr) {
+        new Thread(() ->  {
+            try {
+                Socket socket = new Socket("se2-submission.aau.at", 20080);
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                out.println(matNr);
+                String response = in.readLine();
+
+                runOnUiThread(() -> {
+                    tvResponse.setText(response);
+                    tvResponse.setMovementMethod(new ScrollingMovementMethod());
+                });
+
+                out.close();
+                in.close();
+                socket.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
+    }
+
 }
